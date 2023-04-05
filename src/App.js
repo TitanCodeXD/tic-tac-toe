a56ef5061e012d07d2ce5c19784c77ffc7579817
TitanCodeXD <wesleysantos32892653@outlook.com>
2023-04-04 21:38:36 -0300

    Começando o CSS - Sobre

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-          Edite o <code>src/App.js</code> e <strong>SALVE</strong> para <strong>RECARREGAR</strong>
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprenda React!
-        </a>
-      </header>
-    </div>
+    <a href = "#" className = "action">Sobre</a>
   );
 }
 
