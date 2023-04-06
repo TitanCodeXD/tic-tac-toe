933e2e9fc3776ed8ed8f652a6d6dbd6488aaf281
TitanCodeXD <wesleysantos32892653@outlook.com>
2023-04-06 20:49:55 -0300

    Começando a arquitetura de pastas, usando ITCSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './Styles/Settings/Colors.css';
+import 'normalize.css';
+import './Styles/Generic/Reset.css';
+import './Styles/Elements/Base.css';
 import App from './App';
 
 
