3aaadfd68693a2c38d69636acad6187c449907b1
TitanCodeXD <wesleysantos32892653@outlook.com>
2023-04-12 22:14:42 -0300

    Criando Logo e adicionando ele no App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Sobre from './Objects/Sobre';
+import Logo from './Objects/Logo';
 
 
 function App() {
-  return (<Sobre />);
+  return (
+  <>
+    <Logo />
+    <Sobre />   
+  </>
+  );
 }
 
 export default App;
