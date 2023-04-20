d45202be24724452441d7c88ce35dee95362ceb3
TitanCodeXD <wesleysantos32892653@outlook.com>
2023-04-20 02:26:50 -0300

    Estilizando Componente Header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import './styles.css';
 import Logo from '../../Objects/Logo';
 import Sobre from '../../Objects/Sobre';
 import Menu from '../../Objects/Menu';
 
-
 const Header = () => (
-    <header className = "">
+    <header className = "header">
         <Logo />
         <Sobre />
         <Menu />
