127a1cc457d0a8e4ed078b811be15fe69311e6f3
TitanCodeXD <wesleysantos32892653@outlook.com>
2023-04-20 02:38:05 -0300

    Criado o Object Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Card from './Objects/Card';
 import Header from './Components/Header';
 
 
 function App() {
   return (
+    <>
   <Header/>
+  <Card/>
+  </>
+
   );
 }
 
