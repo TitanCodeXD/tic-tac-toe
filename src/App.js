bd3cba95acc190ff084ced76e1b6dceed46717f4
Wesley Santos <wesleysantos32892653@outlook.com>
2023-07-11 22:09:19 -0300

    Criando o Object Input

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Hashtag from './Components/Hashtag';
 import Header from './Components/Header';
 import Label from './Objects/Label';
+import Input from './Objects/Input';
 
 
 const App = () => (
   <main className = "app">
     <Header />
     <Hashtag />
+    <Input value = "show"/>
     <Label content = "Mostrar Eventos"/>
   </main>
 )
---
+++ src/Objects/Input/index.js
@@ -0,0 +1,7 @@
+import Reacrt from 'react';
+
+const Input = ({value}) => (
+<input type="checkbox" value = {value}/>
+)
+
+export default Input;
