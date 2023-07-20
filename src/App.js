a691115e706938f1be67d13518d408482c87f03d
Wesley Santos <wesleysantos32892653@outlook.com>
2023-07-20 11:43:12 -0300

    Deixar o Type do Input Dinamico

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => (
   <main className = "app">
     <Header />
     <Hashtag />
-    <Input id = "show" value = "show" content = "Mostrar Eventos"/>
+    <Input id = "show" value = "show" type = "checkbox" content = "Mostrar Eventos"/>
   </main>
 )
 
