5af6291250f83fa08a1053feb9683ac0073b7856
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-06 10:59:17 -0300

    Criando o Objet IconSocial

--- src/Components/ProfileUser/index.js
+++ src/Components/ProfileUser/index.js
@@ -3,9 +3,14 @@ import React from 'react'
 
 //
 import AvatarProfile from '../../Objects/AvatarProfile';
+import IconSocial from '../../Objects/IconSocial';
 
 //Imgs
-import Profile from '../../img/Foto Perfil.jpg'
+import Profile from '../../img/Foto Perfil.jpg';
+import GitHub from '../../img/GitHub Icon.png';
+import Linkedin from '../../img/Linkedin Icon.png';
+import Twitter from '../../img/Twitter Icon.png';
+import Instagram from '../../img/Instagram Icon.png';
 
 //CSS
 import './styles.css';
@@ -18,6 +23,12 @@ const ProfileUser = () => {
         <dd className = 'avatar'><AvatarProfile src = {Profile} alt = "Foto de Perfil Wesley Santos"/></dd>
         <dt className = 'title'>Wesley Santos</dt>
         <dd className = 'description'>Estudante de Engenharia de computação com conhecimentos em HTML, CSS, Javascript e bibliotecas como React, com aptidão em inglês intermediário. "Eat, Sleep, Code, Repeat".</dd>
+        <dd className = "socials">
+          <IconSocial src = {GitHub} alt = "Logo da GitHub"/>
+          <IconSocial src = {Linkedin} alt = "Logo do Linkedin"/>
+          <IconSocial src = {Twitter} alt = "Logo do Twitter"/>
+          <IconSocial src = {Instagram} alt = "Logo do Instagram"/>
+        </dd>
     </dl>
 
   )
---
+++ src/Objects/IconSocial/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const IconSocial = ({src, alt}) => {
+
+
+  return (
+    <div>
+        <img className = 'IconSocial' src = {src} alt = {alt}></img>
+    </div>
+  )
+}
+
+export default IconSocial
